06ca3053389433fd77312a99eb36fb183091c43a
Pfftz <Kalixpfft+github@protonmail.com>
2023-08-27 20:14:33 +0700

    export&import js

---
+++ coffemachine/state.js
@@ -0,0 +1,12 @@
+const coffeeStock = {
+    arabica: 100,
+    robusta: 150,
+    liberica: 200
+}
+
+module.exports = coffeeStock;
+
+/*
+untuk melihat nilai dari module pada berkas ini
+console.log(module);
+*/
