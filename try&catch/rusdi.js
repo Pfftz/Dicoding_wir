fb8086fb5bcd7f0cfe8fcc86815b8cde0ce15809
Pfftz <Kalixpfft+github@protonmail.com>
2023-08-28 20:54:18 +0700

    operator_throw

--- try&catch/rusdi.js
+++ try&catch/rusdi.js
@@ -1,11 +1,28 @@
-const json = '{ bad json }';
+// const json = '{ "age": 20 }';
+
+// try {
+//     const user = JSON.parse(json);
+
+//     console.log(user.name); // undefined
+//     console.log(user.age);  // 20
+// } catch (error) {
+//     console.log(error.name);
+//     console.log(error.message);
+// }
+
+// Ketika properti user.name tidak memiliki nilai, maka program akan menghasilkan SyntaxError
+
+const json = '{ "age": 20 }';
 
 try {
     const user = JSON.parse(json);
 
-    console.log(user.name);
-    console.log(user.age);
+    if (!user.name) {
+        throw new SyntaxError("'name' is required.");
+    }
+
+    console.log(user.name); // undefined
+    console.log(user.age);  // 20
 } catch (error) {
-    console.log(error.name);
-    console.log(error.message);
+    console.log(`JSON Error: ${error.message}`);
 }
