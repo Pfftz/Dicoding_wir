07621a037cb222de20f1ab644cf307c0c74fca56
Pfftz <Kalixpfft+github@protonmail.com>
2023-08-28 20:22:15 +0700

    errorhandling

---
+++ try&catch/jawir.js
@@ -0,0 +1,14 @@
+try {
+    console.log("Awal blok try");   // (1)
+    errorCode;                      // (2)
+    console.log("Akhir blok try");  // (3)
+} catch (error) {
+    console.log(error.name);
+    console.log(error.message);
+    console.log(error.stack);
+}
+
+// name: Nama error yang terjadi.
+// message : Pesan tentang detail error.
+// stack : Informasi urutan kejadian yang menyebabkan error.
+// Umumnya digunakan untuk debugging karena terdapat informasi baris mana yang menyebabkan error.
