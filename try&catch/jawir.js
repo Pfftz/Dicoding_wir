e5b911aa7140c1c70b7bf216d7be55794a89c126
Pfftz <Kalixpfft+github@protonmail.com>
2023-08-28 20:30:24 +0700

    try_catch_finally

--- try&catch/jawir.js
+++ try&catch/jawir.js
@@ -1,14 +1,23 @@
-try {
-    console.log("Awal blok try");   // (1)
-    errorCode;                      // (2)
-    console.log("Akhir blok try");  // (3)
-} catch (error) {
-    console.log(error.name);
-    console.log(error.message);
-    console.log(error.stack);
-}
+// try {
+//     console.log("Awal blok try");   // (1)
+//     errorCode;                      // (2)
+//     console.log("Akhir blok try");  // (3)
+// } catch (error) {
+//     console.log(error.name);
+//     console.log(error.message);
+//     console.log(error.stack);
+// }
 
 // name: Nama error yang terjadi.
 // message : Pesan tentang detail error.
 // stack : Informasi urutan kejadian yang menyebabkan error.
 // Umumnya digunakan untuk debugging karena terdapat informasi baris mana yang menyebabkan error.
+
+try {
+    console.log("Awal blok try");
+    console.log("Akhir blok try");
+} catch (error) {
+    console.log("Baris ini diabaikan");
+} finally {
+    console.log("Akan tetap dieksekusi");
+}
