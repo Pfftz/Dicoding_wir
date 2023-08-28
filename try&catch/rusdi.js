0e710fa0ef368866a4440127585746aa8bfc4135
Pfftz <Kalixpfft+github@protonmail.com>
2023-08-28 21:02:34 +0700

    referenceerror & if statement in catch

--- try&catch/rusdi.js
+++ try&catch/rusdi.js
@@ -12,7 +12,22 @@
 
 // Ketika properti user.name tidak memiliki nilai, maka program akan menghasilkan SyntaxError
 
-const json = '{ "age": 20 }';
+// const json = '{ "age": 20 }';
+
+// try {
+//     const user = JSON.parse(json);
+
+//     if (!user.name) {
+//         throw new SyntaxError("'name' is required.");
+//     }
+
+//     console.log(user.name); // undefined
+//     console.log(user.age);  // 20
+// } catch (error) {
+//     console.log(`JSON Error: ${error.message}`);
+// }
+
+const json = '{ "name": "Yoda", "age": 20 }';
 
 try {
     const user = JSON.parse(json);
@@ -21,8 +36,19 @@ try {
         throw new SyntaxError("'name' is required.");
     }
 
-    console.log(user.name); // undefined
+    errorCode;
+
+    console.log(user.name); // Yoda
     console.log(user.age);  // 20
+// } catch (error) {
+//     console.log(`JSON Error: ${error.message}`);
+// }
 } catch (error) {
-    console.log(`JSON Error: ${error.message}`);
+    if (error instanceof SyntaxError) {
+        console.log(`JSON Error: ${error.message}`);
+    } else if (error instanceof ReferenceError) {
+        console.log(error.message);
+    } else {
+        console.log(error.stack);
+    }
 }
