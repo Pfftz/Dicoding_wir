81d2c45d2a00be10416ee19c89bdc544f4eb4f6e
Pfftz <Kalixpfft+github@protonmail.com>
2023-08-28 20:41:11 +0700

    contoh SyntaxError di JSON.parse

---
+++ try&catch/rusdi.js
@@ -0,0 +1,11 @@
+const json = '{ bad json }';
+
+try {
+    const user = JSON.parse(json);
+
+    console.log(user.name);
+    console.log(user.age);
+} catch (error) {
+    console.log(error.name);
+    console.log(error.message);
+}
