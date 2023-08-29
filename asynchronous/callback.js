12c93a1bb0ccf18671b9ec70ad94793ed60a12d7
Pfftz <Kalixpfft+github@protonmail.com>
2023-08-29 08:53:50 +0700

    mengubah getUsers

--- asynchronous/callback.js
+++ asynchronous/callback.js
@@ -1,16 +1,16 @@
-function getUsers(callback) {
-    // simulate network delay
-    setTimeout(() => {
-        const users = ['John', 'Jack', 'Abigail'];
-        callback(users);
-    }, 3000);
-}
+// function getUsers(callback) {
+//     // simulate network delay
+//     setTimeout(() => {
+//         const users = ['John', 'Jack', 'Abigail'];
+//         callback(users);
+//     }, 3000);
+// }
 
-function usersCallback(users) {
-    console.log(users);
-}
+// function usersCallback(users) {
+//     console.log(users);
+// }
 
-getUsers(usersCallback);
+// getUsers(usersCallback);
 
 // function getUsers(isOffline, callback) {
 //     // simulate network delay
@@ -25,3 +25,28 @@ getUsers(usersCallback);
 //         callback(null, users);
 //     }, 3000);
 // }
+
+function getUsers(isOffline, callback) {
+    // simulate network delay
+    setTimeout(() => {
+        const users = ['John', 'Jack', 'Abigail'];
+
+        if (isOffline) {
+            callback(new Error('cannot retrieve users due offline'), null);
+            return;
+        }
+
+        callback(null, users);
+    }, 3000);
+}
+
+function usersCallback(error, users) {
+    if (error) {
+        console.log('process failed:', error.message);
+        return;
+    }
+    console.log('process success:', users);
+}
+
+getUsers(false, usersCallback); // process success: ['John', 'Jack', 'Abigail']
+getUsers(true, usersCallback); // process failed: cannot retrieve users due offline
