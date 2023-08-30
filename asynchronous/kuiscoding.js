b9316573f3cc7d5bcdf4cf324f9005114336bf00
Pfftz <Kalixpfft+github@protonmail.com>
2023-08-30 18:42:23 +0700

    nambahin kuiscoding

---
+++ asynchronous/kuiscoding.js
@@ -0,0 +1,31 @@
+/**
+ * @TODO
+ * Ubahlah fungsi asynchronous callback-based getProvinces menjadi Promise-based.
+ *
+ * Catatan:
+ * - Anda boleh menggunakan util.promisify untuk mengubah fungsi callback-based menjadi Promise-based.
+ * - Jika nama fungsinya berubah, sesuaikan nilai yang diekspor tanpa mengubah nama properti di akhir berkas ini.
+ */
+import { promisify } from 'util';
+
+function getProvinces(countryId, callback) {
+    setTimeout(() => {
+        if (countryId === 'id') {
+            callback(null, [
+                { id: 'id-jk', name: 'Jakarta' },
+                { id: 'id-bt', name: 'Banten' },
+                { id: 'id-jr', name: 'Jawa Barat' },
+            ]);
+            return;
+        }
+
+        callback(new Error('Country not found'), null);
+    }, 1000);
+}
+
+const getPromise = promisify(getProvinces);
+getPromise("id")
+    .then(result => console.log(result))
+    .catch(err => console.log(err.message));
+
+export const getProvinces = getPromise;
