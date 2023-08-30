8f940e7c32e1f33973a434ad990f7dd27719190b
Pfftz <Kalixpfft+github@protonmail.com>
2023-08-30 20:11:27 +0700

    add async function wir

--- chainpromise/nobis.js
+++ chainpromise/nobis.js
@@ -51,12 +51,28 @@ function goInsideCinema(ticket) {
 // }
 
 //versi simplified
-function watchMovie() {
-    withDrawMoney(10)
-        .then((money) => buyCinemaTicket(money))
-        .then((ticket) => goInsideCinema(ticket))
-        .then((result) => console.log(result))
-        .catch((error) => console.log(error.message));
+// function watchMovie() {
+//     withDrawMoney(10)
+//         .then((money) => buyCinemaTicket(money))
+//         .then((ticket) => goInsideCinema(ticket))
+//         .then((result) => console.log(result))
+//         .catch((error) => console.log(error.message));
+// }
+// watchMovie();
+
+//add async function
+async function watchMovie() {
+    try {
+        const money = await withDrawMoney(10);
+        const ticket = await buyCinemaTicket(money);
+        const result = await goInsideCinema(ticket);
+
+        console.log(result);
+    } catch (error) {
+        console.log(error.message);
+    }
 }
 
-watchMovie();
+watchMovie().then(() => console.log('done'));
+
+
