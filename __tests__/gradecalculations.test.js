e6419dd5ade4e55455f678c898adb7079dd28e13
Pfftz <Kalixpfft+github@protonmail.com>
2023-09-02 21:28:46 +0700

    final test

--- __tests__/gradecalculations.test.js
+++ __tests__/gradecalculations.test.js
@@ -1,8 +1,28 @@
-//penulisan unit test untuk kode averageExams
+const { averageExams, isStudentPassExam } = require('../gradecalculations');
 
-const { averageExams } = require('../gradecalculations');
+describe('grade calculations', () => {
+    test('it should return exact average', () => {
+        const listValueOfExams = [80, 100, 100, 80];
+        expect(averageExams(listValueOfExams)).toEqual(90);
+    });
 
-test('it should return exact average', () => {
-    const listValueOfExams = [80, 100, 100, 80];
-    expect(averageExams(listValueOfExams)).toEqual(90);
+    test('it should handle non-number ', () => {
+        const listValueOfExams = [80, 'a', '100', 80];
+        expect(() => averageExams(listValueOfExams)).toThrow();
+    })
+
+    test('it should return exam passed status', () => {
+        const listValueOfExams = [80, 100, 100, 80];
+        expect(isStudentPassExam(listValueOfExams, 'Budi')).toEqual(true);
+    })
+
+    test('it should return exact average', () => {
+        const listValueOfExams = [80, 100, 100, 80];
+        expect(averageExams(listValueOfExams)).toEqual(90);
+    })
+
+    test('it should return exam failed status', () => {
+        const listValueOfExams = [50, 40, 70, 80];
+        expect(isStudentPassExam(listValueOfExams, 'Budi')).toEqual(false);
+    })
 })
